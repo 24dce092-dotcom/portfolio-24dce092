3e8cbe14326ae4213cae9bbb22078d97b7a5261a
24dce092-dotcom <24dce092@charusat.edu.in>
2026-08-20 13:25:38 +0530

    Point Projects API to backend:5001

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const API = 'http://localhost:5000/api';
+const API = 'http://localhost:5001/api';
 
 export default function Projects() {
   const [tasks, setTasks] = useState([]);
